b7dec57197521c81757a4481568a50f493bb6271
Edward Yeh <yhh76227@gmail.com>
2022-09-19 22:10:40 +0800

    refactor: update version information

--- utils/general.py
+++ utils/general.py
@@ -6,7 +6,7 @@
 General function set
 """
 
-PROG_VERSION = 'v0.5.2'
+PROG_VERSION = 'v0.5.3'
 
 def str2int(str_: str, is_signed: bool=False, bits: int=32) -> int:
     """Convert string to integer (with HEX check)"""  #{{{
